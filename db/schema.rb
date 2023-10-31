4fcb1b1656cbdadaec6fd71cb9523f57612bceac
Michael Lopes <gh api loopes.09@gmail.com>
2023-10-31 19:28:41 -0400

    Creation du model Book

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_28_184027) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_232407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "author"
+    t.string "category"
+    t.integer "year"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
