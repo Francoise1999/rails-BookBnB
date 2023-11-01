1d5004bf1f70869c613c8317e1b43cc538c6ce7c
Michael Lopes <gh api loopes.09@gmail.com>
2023-10-31 20:36:56 -0400

    J'ai ajoute des validations simples tel que dans le schema editor

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :category, presence: true
+  validates :year, presence: true
+  validates :price, presence: true
 end
