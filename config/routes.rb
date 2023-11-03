8556d42496fc4c036a908698df950b0efbab56e7
Veronique Tureau <veronique.tureau@gmail.com>
2023-11-02 20:06:59 -0400

    actions new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new_book'
   devise_for :users
+
   root to: "books#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :books, only: [:index, :show, :new, :create] do
